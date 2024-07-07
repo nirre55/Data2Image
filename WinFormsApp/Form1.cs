203d0da06ec3b470812b7b8b60236239bd4da2ff
Mehdi <mehdioulmi@gmail.com>
2024-07-07 18:11:40 -0400

    ajustement

--- WinFormsApp/Form1.cs
+++ WinFormsApp/Form1.cs
@@ -1,10 +1,27 @@
+using Implementation;
+using Implementation.Interfaces;
+
 namespace WinFormsApp
 {
     public partial class Form1 : Form
     {
-        public Form1()
+        private readonly IFileByteReader _fileByteReader;
+        public Form1(IFileByteReader fileByteReader)
         {
+            _fileByteReader = fileByteReader;
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var filePath = @"E:\test.mkv";
+            var result = _fileByteReader.ReadFileBytes(filePath);
+            double ss = result.Length / 3.0;
         }
     }
 }
